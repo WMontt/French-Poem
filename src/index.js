157061a8e976f1ca404c79ea43bb844058da92bb
WMontt <wmonthant11@gmail.com>
2025-06-01 20:07:33 +0630

    update html and js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function generatePoem(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey = "34eba43b83b3fa388doc023be8fb7t33";
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
