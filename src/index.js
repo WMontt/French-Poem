94c538a9c4c79c43bdb7c2860a8d7a54c3bbf57c
WMontt <wmonthant11@gmail.com>
2025-06-01 20:14:31 +0630

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,4 +25,4 @@ function generatePoem(event) {
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem());
+poemFormElement.addEventListener("submit", generatePoem);
